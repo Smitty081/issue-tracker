e5b13a8abe8951e3effba6ca4cad888daba0d54a
smitty081 <jlsmithdevops@gmail.com>
2025-03-13 19:59:58 -0500

    Set up radix ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
+import "@radix-ui/themes/styles.css";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Theme } from "@radix-ui/themes";
 import NavBar from "./NavBar";
 
 const inter = Inter({
@@ -21,8 +23,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.variable}>
-        <NavBar />
-        <main>{children}</main>
+        <Theme>
+          <NavBar />
+          <main>{children}</main>
+        </Theme>
       </body>
     </html>
   );
